c97daa02da941fd39d9188074c795ed03a4f5da1
maning <ning.ma@12301.cn>
2017-04-12 10:20:29 +0800

    收藏分类

--- app/src/main/java/com/maning/gankmm/ui/adapter/RecycleCollectAdapter.java
+++ app/src/main/java/com/maning/gankmm/ui/adapter/RecycleCollectAdapter.java
@@ -2,6 +2,7 @@ package com.maning.gankmm.ui.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +28,7 @@ import butterknife.ButterKnife;
 /**
  * Created by maning on 16/5/17.
  */
-public class RecycleCollectAdapter extends RecyclerView.Adapter<RecycleCollectAdapter.MyViewHolder> {
+public class RecycleCollectAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context context;
     private List<GankEntity> commonDataResults;
@@ -41,84 +42,140 @@ public class RecycleCollectAdapter extends RecyclerView.Adapter<RecycleCollectAd
 
     private OnItemClickLitener mOnItemClickLitener;
 
-    public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener)
-    {
+    public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener) {
         this.mOnItemClickLitener = mOnItemClickLitener;
     }
 
-    public void updateDatas(List<GankEntity> commonDataResults){
+    public void updateDatas(List<GankEntity> commonDataResults) {
         this.commonDataResults = commonDataResults;
         notifyDataSetChanged();
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == 0) {
+            View inflate = layoutInflater.inflate(R.layout.item_collect_pic, parent, false);
+            return new MyViewHolder(inflate);
+        } else {
+            View inflate = layoutInflater.inflate(R.layout.item_collect, parent, false);
+            return new MyViewHolder1(inflate);
+        }
 
-        View inflate = layoutInflater.inflate(R.layout.item_collect, parent, false);
-
-        return new MyViewHolder(inflate);
     }
 
     @Override
-    public void onBindViewHolder(final MyViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder viewHolder, final int position) {
 
         final GankEntity resultsEntity = commonDataResults.get(position);
 
-        viewHolder.tvShowWho.setText(resultsEntity.getWho());
-        viewHolder.tvShowTitle.setText(resultsEntity.getDesc());
-        viewHolder.tvShowTime.setText(resultsEntity.getPublishedAt().split("T")[0]);
+        if (viewHolder instanceof MyViewHolder1) {
+            final MyViewHolder1 myViewHolder1 = (MyViewHolder1) viewHolder;
 
-        //是福利的话就展示图片
-        if (Constants.FlagWelFare.equals(resultsEntity.getType())) {
-            viewHolder.image.setVisibility(View.VISIBLE);
-            viewHolder.tvShowTitle.setVisibility(View.GONE);
+            myViewHolder1.tvShowTitle.setText(resultsEntity.getDesc());
+            myViewHolder1.tvShowWho.setText(resultsEntity.getWho());
+            myViewHolder1.tvShowTime.setText(resultsEntity.getPublishedAt().split("T")[0]);
+
+            //图片展示
+            String imageUrl = "";
+            List<String> images = resultsEntity.getImages();
+            if (images != null && images.size() > 0) {
+                imageUrl = images.get(0);
+            }
+            if (TextUtils.isEmpty(imageUrl)) {
+                myViewHolder1.ivShow.setVisibility(View.GONE);
+            } else {
+                myViewHolder1.ivShow.setVisibility(View.VISIBLE);
+                Glide.with(context)
+                        .load(imageUrl)
+                        .placeholder(R.drawable.pic_gray_bg)
+                        .centerCrop()
+                        .into(myViewHolder1.ivShow);
+            }
+
+            //查询是否存在收藏
+            boolean isCollect = new CollectDao().queryOneCollectByID(resultsEntity.get_id());
+            if (isCollect) {
+                myViewHolder1.btnCollect.setLiked(true);
+            } else {
+                myViewHolder1.btnCollect.setLiked(false);
+            }
+
+            myViewHolder1.btnCollect.setOnLikeListener(new OnLikeListener() {
+                @Override
+                public void liked(LikeButton likeButton) {
+                    boolean insertResult = new CollectDao().insertOneCollect(resultsEntity);
+                    if (insertResult) {
+                        MySnackbar.makeSnackBarBlack(myViewHolder1.tvShowTime, "收藏成功");
+                    } else {
+                        MySnackbar.makeSnackBarRed(myViewHolder1.tvShowTime, "收藏失败");
+                        likeButton.setLiked(false);
+                    }
+                }
+
+                @Override
+                public void unLiked(LikeButton likeButton) {
+                    boolean deleteResult = new CollectDao().deleteOneCollect(resultsEntity.get_id());
+                    if (deleteResult) {
+                        MySnackbar.makeSnackBarBlack(myViewHolder1.tvShowTime, "取消收藏成功");
+                    } else {
+                        MySnackbar.makeSnackBarRed(myViewHolder1.tvShowTime, "取消收藏失败");
+                        likeButton.setLiked(true);
+                    }
+
+                }
+            });
+
+
+        } else if (viewHolder instanceof MyViewHolder) {
+            final MyViewHolder myViewHolder = (MyViewHolder) viewHolder;
+
+            myViewHolder.tvShowWho.setText(resultsEntity.getWho());
+            myViewHolder.tvShowTime.setText(resultsEntity.getPublishedAt().split("T")[0]);
+
             String url = resultsEntity.getUrl();
-            KLog.i("图片地址："+url);
+            KLog.i("图片地址：" + url);
             //图片显示
             Glide.with(context)
                     .load(url)
                     .placeholder(R.drawable.pic_gray_bg)
                     .error(R.drawable.pic_gray_bg)
                     .centerCrop()
-                    .into(viewHolder.image);
+                    .into(myViewHolder.image);
 
-        } else {
-            viewHolder.image.setVisibility(View.GONE);
-            viewHolder.tvShowTitle.setVisibility(View.VISIBLE);
-        }
-
-        //查询是否存在收藏
-        boolean isCollect = new CollectDao().queryOneCollectByID(resultsEntity.get_id());
-        if (isCollect) {
-            viewHolder.btnCollect.setLiked(true);
-        } else {
-            viewHolder.btnCollect.setLiked(false);
-        }
-
-        viewHolder.btnCollect.setOnLikeListener(new OnLikeListener() {
-            @Override
-            public void liked(LikeButton likeButton) {
-                boolean insertResult = new CollectDao().insertOneCollect(resultsEntity);
-                if (insertResult) {
-                    MySnackbar.makeSnackBarBlack(viewHolder.tvShowTime, "收藏成功");
-                } else {
-                    MySnackbar.makeSnackBarRed(viewHolder.tvShowTime, "收藏失败");
-                    likeButton.setLiked(false);
-                }
+            //查询是否存在收藏
+            boolean isCollect = new CollectDao().queryOneCollectByID(resultsEntity.get_id());
+            if (isCollect) {
+                myViewHolder.btnCollect.setLiked(true);
+            } else {
+                myViewHolder.btnCollect.setLiked(false);
             }
 
-            @Override
-            public void unLiked(LikeButton likeButton) {
-                boolean deleteResult = new CollectDao().deleteOneCollect(resultsEntity.get_id());
-                if (deleteResult) {
-                    MySnackbar.makeSnackBarBlack(viewHolder.tvShowTime, "取消收藏成功");
-                } else {
-                    MySnackbar.makeSnackBarRed(viewHolder.tvShowTime, "取消收藏失败");
-                    likeButton.setLiked(true);
+            myViewHolder.btnCollect.setOnLikeListener(new OnLikeListener() {
+                @Override
+                public void liked(LikeButton likeButton) {
+                    boolean insertResult = new CollectDao().insertOneCollect(resultsEntity);
+                    if (insertResult) {
+                        MySnackbar.makeSnackBarBlack(myViewHolder.tvShowTime, "收藏成功");
+                    } else {
+                        MySnackbar.makeSnackBarRed(myViewHolder.tvShowTime, "收藏失败");
+                        likeButton.setLiked(false);
+                    }
                 }
 
-            }
-        });
+                @Override
+                public void unLiked(LikeButton likeButton) {
+                    boolean deleteResult = new CollectDao().deleteOneCollect(resultsEntity.get_id());
+                    if (deleteResult) {
+                        MySnackbar.makeSnackBarBlack(myViewHolder.tvShowTime, "取消收藏成功");
+                    } else {
+                        MySnackbar.makeSnackBarRed(myViewHolder.tvShowTime, "取消收藏失败");
+                        likeButton.setLiked(true);
+                    }
+
+                }
+            });
+
+        }
 
         //如果设置了回调，则设置点击事件
         if (mOnItemClickLitener != null) {
@@ -133,11 +190,38 @@ public class RecycleCollectAdapter extends RecyclerView.Adapter<RecycleCollectAd
     }
 
     @Override
+    public int getItemViewType(int position) {
+        if (Constants.FlagWelFare.equals(commonDataResults.get(position).getType())) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
     public int getItemCount() {
         return commonDataResults.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.tvShowWho)
+        TextView tvShowWho;
+        @Bind(R.id.tvShowTime)
+        TextView tvShowTime;
+        @Bind(R.id.btn_collect)
+        LikeButton btnCollect;
+        @Bind(R.id.image)
+        ImageView image;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+
+    static class MyViewHolder1 extends RecyclerView.ViewHolder {
 
         @Bind(R.id.tvShowWho)
         TextView tvShowWho;
@@ -147,10 +231,10 @@ public class RecycleCollectAdapter extends RecyclerView.Adapter<RecycleCollectAd
         TextView tvShowTime;
         @Bind(R.id.btn_collect)
         LikeButton btnCollect;
-        @Bind(R.id.image)
-        ImageView image;
+        @Bind(R.id.iv_show)
+        ImageView ivShow;
 
-        public MyViewHolder(View itemView) {
+        public MyViewHolder1(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
