3bce9bbfb5fd61ab9b2e50bc4289afba526f915c
maning <ning.ma@12301.cn>
2017-06-30 17:23:23 +0800

    天气页面代码判断增加判断,多谢用户的反馈! 修复部分时间段出现天气页面崩溃现象

--- app/src/main/java/com/maning/gankmm/ui/adapter/Weather2Adapter.java
+++ app/src/main/java/com/maning/gankmm/ui/adapter/Weather2Adapter.java
@@ -1,9 +1,8 @@
 package com.maning.gankmm.ui.adapter;
 
 import android.content.Context;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,15 +44,46 @@ public class Weather2Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         if (holder instanceof MyViewHolder01) {
             final MyViewHolder01 myViewHolder01 = (MyViewHolder01) holder;
 
+
+            myViewHolder01.tv_01.setText("--");
+            myViewHolder01.tv_02.setText("--");
+            myViewHolder01.tv_03.setText("--");
+            myViewHolder01.tv_04.setText("--");
+            myViewHolder01.tv_05.setText("--");
+            myViewHolder01.tv_06.setText("--");
+            myViewHolder01.tv_07.setText("--");
+            myViewHolder01.tv_08.setText("--");
+
+
             WeatherBeseEntity.WeatherBean.FutureBean futureBean = weatherEntity.getFuture().get(position);
             myViewHolder01.tv_01.setText(futureBean.getWeek());
             myViewHolder01.tv_02.setText(futureBean.getDate().substring(5));
             myViewHolder01.tv_03.setText(futureBean.getDayTime());
-            myViewHolder01.tv_04.setText(futureBean.getTemperature().split("/")[0].replace(" ", ""));
-            myViewHolder01.tv_05.setText(futureBean.getTemperature().split("/")[1].replace(" ", ""));
+            //最高温度和最低温度
+            String temperature = futureBean.getTemperature();
+            if(!TextUtils.isEmpty(temperature)){
+                if(temperature.length() > 1 && temperature.contains("/")){
+                    String[] tempers = temperature.split("/");
+                    if(tempers.length > 0){
+                        myViewHolder01.tv_04.setText(tempers[0].replace(" ", ""));
+                        myViewHolder01.tv_05.setText(tempers[1].replace(" ", ""));
+                    }
+                }
+            }
+
             myViewHolder01.tv_06.setText(futureBean.getNight());
-            myViewHolder01.tv_07.setText(futureBean.getWind().split(" ")[0].replace(" ", ""));
-            myViewHolder01.tv_08.setText(futureBean.getWind().split(" ")[1].replace(" ", ""));
+
+            //风向和风速
+            String wind = futureBean.getWind();
+            if(!TextUtils.isEmpty(wind)){
+                if(wind.length() > 1 && wind.contains(" ")){
+                    String[] winds = wind.split(" ");
+                    if(winds.length > 0){
+                        myViewHolder01.tv_07.setText(winds[0].replace(" ", ""));
+                        myViewHolder01.tv_08.setText(winds[1].replace(" ", ""));
+                    }
+                }
+            }
 
             myViewHolder01.iv_01.setImageDrawable(mContext.getResources().getDrawable(SharePreUtil.getIntData(mContext, futureBean.getDayTime(), R.drawable.icon_weather_none)));
             myViewHolder01.iv_02.setImageDrawable(mContext.getResources().getDrawable(SharePreUtil.getIntData(mContext, futureBean.getNight(), R.drawable.icon_weather_none)));
